068aed2fe03249ed698d30d74b3e9761db630c24
tsukushi-wakige <tsukushi2725899@gmail.com>
2024-06-29 03:20:01 +0900

    ノベル追加

---
+++ server/api/novel/controller.ts
@@ -0,0 +1,6 @@
+import { defineController } from './$relay';
+import { novelUseCase } from 'domain/novel/useCase/novelUseCase';
+export default defineController(() => ({
+  get: () => ({ status: 200, body: 'Hello' }),
+  post : async ({ body }) => ({ status: 200, body: await novelUseCase.scrape( body.aozoraUrl ) }),
+}));
